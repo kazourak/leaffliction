97826cfc001231ff7313626752a64a9f9bca9bfc
nskiba-sub <nolan.skiba@student.42angouleme.fr>
2024-11-13 15:49:40 +0100

    3-data-augmentation-issue: add test for augmentation

---
+++ tests/augmentation/test_augmentation.py
@@ -0,0 +1,32 @@
+import pathlib
+
+import cv2
+import sys
+
+sys.path.append(str(pathlib.Path(__file__).resolve().parents[2]))
+from leaffliction.augmentation.Augmentation import augment
+
+
+def test_augmentation() -> None:
+    """
+    Test the augmentation function.
+
+    Returns
+    -------
+
+    """
+    image = cv2.imread('tests/augmentation/test_image.png')
+
+    if image is None:
+        assert False
+
+    augmented_images = augment(image, 0.5)
+
+    assert augmented_images is not None
+
+    # check if number of augmented images is correct
+    assert len(augmented_images) == 6
+
+    # check if all images are not None
+    for augmented_image in augmented_images.values():
+        assert augmented_image is not None
